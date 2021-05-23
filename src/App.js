6a8a70fb5242f1545da23e57108562306824f2e8
cpstuardo <cpstuardo@uc.cl>
2021-05-23 00:00:11 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className = "App">
+        <div className = "header">
+          <h1>Bienvenido a Vuelos</h1>
+          <form>
+            <button>Solicitar información de vuelos</button>
+          </form>
+        </div>
+      </div>
   );
 }
 
