a5b2dbda0eecd673d0037326d52ed7703cd2457d
cpstuardo <cpstuardo@uc.cl>
2021-05-23 00:43:10 -0400

    Updates

---
+++ precache-manifest.a6ed3af60882c233326265b2443fc187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "029c6eb8905683714021",
+    "url": "/t3_ti/static/css/main.1ba0ec77.chunk.css"
+  },
+  {
+    "revision": "029c6eb8905683714021",
+    "url": "/t3_ti/static/js/main.da65cce0.chunk.js"
+  },
+  {
+    "revision": "2cd78484752b87521cce",
+    "url": "/t3_ti/static/js/runtime~main.1f70e262.js"
+  },
+  {
+    "revision": "9aafd7bfa63eb7e35d8e",
+    "url": "/t3_ti/static/css/2.a8fe0c74.chunk.css"
+  },
+  {
+    "revision": "9aafd7bfa63eb7e35d8e",
+    "url": "/t3_ti/static/js/2.fce2ae3c.chunk.js"
+  },
+  {
+    "revision": "c2f96ea761773c33a9de",
+    "url": "/t3_ti/static/js/3.6aaeffcc.chunk.js"
+  },
+  {
+    "revision": "c462d5196d987baa7307246a40ec07c6",
+    "url": "/t3_ti/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/t3_ti/precache-manifest.a211174375dc2a0f33ee1f99ad30cfb8.js"
+  "/t3_ti/precache-manifest.a6ed3af60882c233326265b2443fc187.js"
 );
 
 workbox.clientsClaim();
