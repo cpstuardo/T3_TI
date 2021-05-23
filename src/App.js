155c29770f8cc0bca9f91b0cad6967bf9c0e9acc
cpstuardo <cpstuardo@uc.cl>
2021-05-23 00:23:03 -0400

    chat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import socket from './components/Socket';
+import TextField from '@material-ui/core/TextField' ;
 
 function App() {
   const [vuelos, setVuelos] = useState([{code:'Sin Información', airline:'', origin:[-100,-100], destination:[-100,-100], plane:'', seats: 1, passengers:[{name:'', age:1}]}]);
-  
+  const [state, setState] = useState({name:'', message:''});
+  const [chat, setChat] = useState([]);
+
   useEffect(() => {
+    socket.on('CHAT', ({name, message, date}) => {
+      setChat([...chat, {name, message, date}])
+    }) 
+
     socket.on('FLIGHTS', data => {
       setVuelos(data)
     })  
   }) 
 
+  // CHAT
+  const messageSubmit = (e) => {
+    e.preventDefault()
+    const {name, message} = state
+    socket.emit('CHAT', {name: name, message: message})
+    setState({message: '', name: ''})
+  }
+  
+  const onTextChange = e => {
+    setState({...state, [e.target.name]: e.target.value})
+  }
+
+  const renderChat = () => {
+    return chat.map(({name, message, date}, index) => (
+      <div key={index}>
+        <b>{name}: </b>{message} - <font size="2" color="teal"><em>{(new Date(date)).toLocaleString()}</em></font>
+      </div>
+    ))
+  }
+  
   const getVuelos = (e) => {
     e.preventDefault()
     socket.emit('FLIGHTS')
   }
-
+  
   const renderVuelos = () => {
     return vuelos.map(({code, airline, origin, destination, plane, seats, passengers}, index) => (
       <div key={index}>
@@ -52,9 +79,26 @@ function App() {
             <button>Solicitar información de vuelos</button>
           </form>
         </div>
-
-
-
+        <div>
+          <div className= "column-mapa">
+          </div>
+        </div>
+        <div className="column-chat">
+              <h2>Centro de control</h2>
+              <div className="container">
+                {renderChat()}
+              </div>
+            <form onSubmit = {messageSubmit}>
+                <div className = "name_field">
+                  <TextField name= "name" onChange={e => onTextChange(e)} value={state.name} label="name" variant="outlined"/>
+                </div>
+                <br></br>
+                <div className = "message_field">
+                  <TextField name= "message" onChange={e => onTextChange(e)} value={state.message} label="message" id="outlined-multiline-static" variant="outlined" />
+                </div>
+                <button>Enviar</button>
+            </form>
+          </div>
         <div className="column-info">
           <h2>Información de vuelos</h2>
           {renderVuelos()}
